51fbeb7738b2cf1d41b51824a8a242db93518476
Ruth Gourarie <ruthashleigh@hotmail.com>
2020-10-31 14:48:43 +0000

    add avatar icon

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import add from "../../assets/add.svg";
 import menu from "../../assets/menu.svg";
+import avatar from "../../assets/avatar.svg";
 
 type Props = {
 
@@ -16,7 +17,9 @@ const Header: React.FC<Props> = ({}) => {
         </div>
       </div>
       <div className="AvatarContainer">
-        <div className="Avatar"></div>
+        <div className="Avatar">
+          <img src={avatar} className="AvatarIcon" />
+        </div>
       </div>
     </div>
   )
